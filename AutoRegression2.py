0faee081f67843bd875f728a2e03414cbd0e9d83
Moorboor <moorboory@gmail.com>
2023-06-20 14:56:27 +0200

    Fixing Indexing error in AutoRegression2.py

--- AutoRegression2.py
+++ AutoRegression2.py
@@ -2,11 +2,12 @@ import os
 
 import numpy as np
 from scipy import special
-import pandas as pd
 
+import pandas as pd
 from tqdm import tqdm
-rng = np.random.default_rng(11)
 import time
+
+rng = np.random.default_rng(11)
 
 
 
@@ -29,7 +30,7 @@ dataset = datasets[dname]
 
 class AR():
 
-    def __init__(self, *, p, train_per, gust_th=1.6, beta=1):
+    def __init__(self, *, p, train_per, gust_th=1.5, beta=1):
 
         self.p = p
         self.train_per = train_per
@@ -38,8 +39,8 @@ class AR():
 
     def get_rolling(self, *, data, mode, show):
 
-        indices = np.arange(0, len(data)-self.train_per+1)
-        n_splits = int(len(data) / 200000)+1
+        indices = np.arange(len(data)-self.train_per+1)
+        n_splits = max(int(len(data) / 2000), 1)
         indices_split = np.array_split(indices, n_splits)
 
         rolling = []
@@ -47,32 +48,17 @@ class AR():
             indices = indices[:, None] + np.arange(self.train_per)
             x = data[indices]
 
-            if mode == "std":
-                split_rolling = x.std(axis=1)
+            if mode == "var":
+                split_rolling = np.nanvar(x, axis=1)
             elif mode == "mean":
-                split_rolling = x.mean(axis=1)
-
-            nan_pos = np.isnan(split_rolling)
-            # TODO CASE: n - n_nans = 1
-            if np.any(nan_pos): 
-                if mode == "std":
-                    nan_rolling = np.apply_along_axis((
-                        lambda data: data[~np.isnan(data)].std() if np.isnan(data).any() else 0), axis=1, arr=x[nan_pos, :]
-                        )
-                elif mode == "mean":
-                    nan_rolling = np.apply_along_axis((
-                        lambda data: data[~np.isnan(data)].mean() if np.isnan(data).any() else 0), axis=1, arr=x[nan_pos, :]
-                        )
-
-                split_rolling = np.nan_to_num(split_rolling, copy=False, nan=0.0)
-                split_rolling[nan_pos] += nan_rolling
+                split_rolling = np.nanmean(x, axis=1) 
                 
             rolling.append(split_rolling)
             
         return np.concatenate(rolling)
 
     
-    def get_phis(self, *, data, stds):
+    def get_phis(self, *, data, var):
         
         def get_yule_walker(*, acfs):
             matrices = acfs[matrix_mask, :].reshape(self.p,self.p,-1).T
@@ -82,39 +68,58 @@ class AR():
         
         # TODO Implement substract mean Steinerscher Verschiebungssatz
 
-        z = [np.array(data[lag:] * data[:-lag])[:lag-self.p-1] for lag in range(1, self.p+1)]
+        z = [np.array(data[lag:] * data[:-lag])[:len(data)-self.p] for lag in range(1, self.p+1)]
         acfs = [self.get_rolling(data=z[lag], mode="mean", show=False) for lag in range(self.p)]
-        acfs = [np.divide(acf, stds**2, out=np.zeros_like(acf), where=stds!= 0) for acf in acfs]
+        acfs = [np.divide(acf, var, out=np.zeros_like(acf), where=var!=0) for acf in acfs]
         acfs.insert(0, np.ones_like(acfs[0]))
         acfs = np.stack(acfs)
 
         matrix_mask = np.array([abs(-k+j) for k in range(self.p) for j in range(self.p)])
-        splits = int(len(acfs[0]) / 200000)+1
+        splits = max(int(len(acfs[0]) / 60000000), 1)
         phis = [get_yule_walker(acfs=acfs_split) for acfs_split in np.array_split(acfs, splits, axis=1)]
 
         return np.concatenate(phis)
     
 
-    def forecast(self, *, data, phis, stds):
+    def forecast(self, *, data, phis, var):
 
-        indices = np.arange(self.train_per-1, len(data))[:, None] - np.arange(self.p)
-        x = data[indices]
-
-        predictions = x[self.p:-1] * phis
+        indices = np.arange(self.p+self.train_per-1, len(data))[:, None] - np.arange(self.p)
+        predictions = data[indices] * phis
         predictions = predictions.sum(axis=1)
 
-        white_noise = rng.normal(scale=(stds**2))
+        white_noise = rng.normal(scale=var)
         predictions += white_noise
             
         return predictions
     
 
-    def get_integrals(self, *, predictions, stds):
+    def get_integrals(self, *, predictions, var):
         
-        z = (self.gust_th - predictions) / ((2*stds**2)**0.5)
-        return 0.5 * (1 - special.erf(z))
-    
+        z = (self.gust_th - predictions) / (2*var)**0.5
+        predictions = 0.5 * (1 - special.erf(z))
+        return predictions#np.where(var==0, 0, predictions)
 
+
+def data_chunks(*, ar, data, var):
+
+    chunk_size = 60000000
+    n_chunks = max(int(len(data)/chunk_size), 1)
+    data_indices = np.arange(len(data)-ar.train_per-ar.p) 
+    var_indices = np.arange(ar.p, len(var)) 
+    data_indices_splits = np.array_split(data_indices, n_chunks)
+    var_indices_splits = np.array_split(var_indices, n_chunks)
+
+    data_indices_splits = [np.append(indices_split, indices_split[-1] + np.arange(1, ar.train_per+ar.p+1)) for indices_split in data_indices_splits]
+    predictions = []
+    for data_indices_chunk, var_indices_chunk in tqdm(zip(data_indices_splits, var_indices_splits), total=len(data_indices_splits)):
+        data_chunk = data[data_indices_chunk]
+        var_chunk = var[var_indices_chunk]
+
+        phis = ar.get_phis(data=data_chunk, var=var_chunk)
+        prediction = ar.forecast(data=data_chunk, phis=phis, var=var_chunk)
+        predictions.append(ar.get_integrals(predictions=prediction, var=var_chunk)) 
+        
+    return np.concatenate(predictions)
 
 
 def get_performances(*, data, predictions, p, train_per, gust_th):
@@ -140,7 +145,6 @@ def get_performances(*, data, predictions, p, train_per, gust_th):
         performances[quantile] = (true_positive_rate, false_positive_rate)
 
     return performances
-
 
 def get_xi(*, performances):
     
@@ -173,71 +177,47 @@ def save_performances(*, performances, p, train_per, gust_th, xi, beta):
 
     data = list(zip(quantiles, true_positive_rates, false_negative_rates))
     df = pd.DataFrame(data, columns=["quantiles", "true_positive_rate", "false_negative_rate"])
-    PERFORMANCE_PATH = os.path.join(ARRAY_PATH, f"{dname}", f"AutoRegression2", f"{p}", f"{gust_th}", f"{beta}")
+    PERFORMANCE_PATH = os.path.join(ARRAY_PATH, f"{dname}", f"AutoRegression2", f"AR({p})", f"GustTh={gust_th}", f"Beta={beta}")
     os.makedirs(PERFORMANCE_PATH, exist_ok=True)
 
     return df.to_csv(os.path.join(PERFORMANCE_PATH, f"{dname}-p={p}-training_per={train_per}-gust_th={gust_th}-beta={beta}-xi={xi:.4f}.csv"))
 
 
-def data_chunks(*, ar, data, stds):
-
-    chunk_size = 5000000
-    n_chunks = int(len(data)/chunk_size)+1
-    data_indices = np.arange(len(data)-ar.train_per-ar.p)
-    stds_indices = np.arange(ar.p, len(stds)-1)
-    data_indices_splits = np.array_split(data_indices, n_chunks)
-    stds_indices_splits = np.array_split(stds_indices, n_chunks)
-
-    data_indices_splits = [np.append(indices_split, indices_split[-1] + np.arange(1, ar.train_per+ar.p+1)) for indices_split in data_indices_splits]
-    predictions = []
-    for data_indices_chunk, stds_indices_chunk in tqdm(zip(data_indices_splits, stds_indices_splits), total=len(data_indices_splits)):
-        data_chunk = data[data_indices_chunk]
-        stds_chunk = stds[stds_indices_chunk]
-
-        phis = ar.get_phis(data=data_chunk, stds=stds_chunk)
-        prediction = ar.forecast(data=data_chunk, phis=phis, stds=stds_chunk)
-        predictions.append(ar.get_integrals(predictions=prediction, stds=stds_chunk)) 
-        
-    return np.concatenate(predictions)
-
-
-dataset = dataset
 gust_ths = [1.5]
 train_pers = np.unique(np.round(10**np.linspace(np.log10(10), np.log10(60*60), 100))).astype(int)
-train_pers = [20]
-p_parameters = [20]
+p_parameters = [2]
 betas = [1] # 0.644
 
 n = len(gust_ths) * len(train_pers) * len(p_parameters) * len(betas)
 i = 0
 
-start = time.time()
+
 
 ar = AR(p=None, train_per=None, gust_th=1, beta=1)
 
-for gust_th in gust_ths:
-    ar.gust_th = gust_th
-    for beta in betas:
-        dataset_gauss = np.sign(dataset) * np.abs(dataset)**beta
-        ar.beta = beta
 
-        for train_per in train_pers:
-            ar.train_per = train_per
+for beta in betas:
+    dataset_gauss = np.sign(dataset) * np.abs(dataset)**beta
+    ar.beta = beta
 
-            print(f"Getting rolling standard deviations tau=({train_per}): ")
-            stds = ar.get_rolling(data=dataset, mode="std", show=True)
+    for train_per in train_pers:
+        ar.train_per = train_per
+        print(f"Getting rolling variances tau=({train_per}): ")
+        var = ar.get_rolling(data=dataset_gauss[:-1], mode="var", show=True)
 
-            for p in tqdm(p_parameters):
+        for p in tqdm(p_parameters):
+            ar.p = p 
+            predictions = data_chunks(ar=ar, data=dataset_gauss[:-1], var=var)
 
-                ar.p = p 
+            for gust_th in gust_ths:
+                start = time.time()
                 i += 1
-                
-                print(f"\n({i}/{n}): Getting AR({p}) phi parameters: ")
-                predictions = data_chunks(ar=ar, data=dataset_gauss, stds=stds)
+                print(f"\n({i}/{n}): Getting AR({p}) predictions")
+                ar.gust_th = gust_th
                 performances = get_performances(data=dataset, predictions=predictions, p=p, train_per=train_per, gust_th=gust_th)
                 xi = get_xi(performances=performances)
-                
+            
                 save_performances(performances=performances, p=p,train_per=train_per, gust_th=gust_th, xi=xi, beta=beta)
 
-end = time.time()
-print(f"{np.round(end-start, 2)}")
+                end = time.time()
+                print(f"AR({p}), tau={train_per}, {np.round(end-start, 1)} secs.")
